990732f685185001ab2df40cc5a9fb18cd2bdc54
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2020-10-01 17:51:03 +0300

    feat: add min, max and avg solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+let isArrayEmptyOrInvalid = (array) => {
+    if (array === undefined || array.length === 0) {
+        return true;
+    }
+    if (!array.some((element) => typeof element === "number")) {
+        return true;
+    }
+    return false;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (isArrayEmptyOrInvalid(array)) {
+        return 0;
+    }
+    return array.reduce((a, b) => Math.min(a, b));
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (isArrayEmptyOrInvalid(array)) {
+        return 0;
+    }
+    return array.reduce((a, b) => Math.max(a, b));
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (isArrayEmptyOrInvalid(array)) {
+        return 0;
+    }
+    return array.reduce((a, b) => a + b, 0) / array.length;
+};
